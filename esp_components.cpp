f08fe2bf3f9bceba3b3642dded25a44465510542
Maikeu Locatelli <maikramer@gmail.com>
2025-11-20 12:26:18 -0300

    Add esp_components.cpp file to implement a wrapper component for automatic subcomponent detection. This file includes documentation and notes on the lack of its own implementation, as subcomponents are automatically detected from specified directories.

---
+++ esp_components.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file esp_components.cpp
+ * @brief Implementação wrapper para o componente esp_components
+ * 
+ * Este é um componente wrapper que agrupa vários subcomponentes.
+ * Os subcomponentes são detectados automaticamente pelo ESP-IDF.
+ */
+
+#include "esp_components.h"
+
+// Este componente wrapper não tem implementação própria
+// Os subcomponentes são detectados automaticamente pelos diretórios:
+// BluetoothServer, Connection, Drivers, ErrorCodes, JsonModels, etc.
